199a06bbe7eb0de08802e7c61fcaa999d9170a88
Okawa <pandanyanya@gmail.com>
2021-10-08 19:16:37 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
@@ -19,5 +19,4 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
-
 end
