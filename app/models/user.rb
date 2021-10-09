03ed02999bd05737195f4979dd62342083cc9fd8
Okawa <pandanyanya@gmail.com>
2021-10-09 14:32:05 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
