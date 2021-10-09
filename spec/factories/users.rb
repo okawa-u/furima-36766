aa1c7b10133fdeb431d81ed85f2f649ce5e7ebca
Okawa <pandanyanya@gmail.com>
2021-10-09 17:49:42 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,13 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
+    password { ‘a1’ + Faker::Internet.password(min_length: 4) }
     password { password }
     password_confirmation { password }
+    last_name
+    first_name
+    last_name_kana
+    first_name_kana
+    birth
   end
 end
