2183d84620696708094f94aa25ed0327e60d5ff9
Okawa <pandanyanya@gmail.com>
2021-10-09 13:38:18 +0900

    テストコードの実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_10_05_091549) do
 
-ActiveRecord::Schema.define(version: 2021_10_04_194014) do
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
