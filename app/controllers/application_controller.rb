a7b96bc1b1059e07428ec3d561a86d8b79719db0
Okawa <pandanyanya@gmail.com>
2021-10-11 01:52:13 +0900

    Active Storageの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :orders
+  has_one_attached :imgage
 end
