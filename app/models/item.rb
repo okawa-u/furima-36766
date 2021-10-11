3dd14201d442918956cf6d28dcb887bad4acd47e
Okawa <pandanyanya@gmail.com>
2021-10-11 21:27:51 +0900

    モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,23 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :days
+
+  validates :category, presence: true
+  validates :status, presence: true
+  validates :fee, presence: true
+  validates :area, presence: true
+  validates :days, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'furima/index'
   root to: "furima#index"
+  resources :items
 end
