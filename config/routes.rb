aa04b2799939fe781bb0451ef93dd06debbca0fb
Okawa <pandanyanya@gmail.com>
2021-10-12 21:59:03 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/new'
   devise_for :users
   get 'furima/index'
+  
   root to: "furima#index"
+  get 'items-３３new', to: 'items#new'
   resources :items
 end
