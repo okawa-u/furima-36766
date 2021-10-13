6433be3cc7caad06f034ff72ff098c3af9245d93
Okawa <pandanyanya@gmail.com>
2021-10-13 21:47:06 +0900

    テストコードの記載

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item do
+    content {Faker::Lorem.sentence}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
