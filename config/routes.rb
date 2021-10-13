c93670c56f404d9cddc6802ac981b8dc4a60a696
Okawa <pandanyanya@gmail.com>
2021-10-13 15:30:21 +0900

    ログイン有無によるルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
   devise_for :users
-  get 'furima/index'
   
   root to: "furima#index"
-  get 'items-３３new', to: 'items#new'
+
+  get 'items-new', to: 'items#new'
   resources :items
 end
