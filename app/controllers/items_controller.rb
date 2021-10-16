1ea3bd174ba9d45f09ab272efdf6a0768d78762b
Okawa <pandanyanya@gmail.com>
2021-10-16 16:45:43 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:create]
   def index
-    # @item = Item.order('created_at DESC')
   end
 
-  before_action :authenticate_user!
   def new
     @item = Item.new
   end
