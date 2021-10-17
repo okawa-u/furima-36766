fa02013bf83bf2b384959a0cd0661c42ad2ed5e9
Okawa <pandanyanya@gmail.com>
2021-10-17 19:20:30 +0900

    Revert "ルーティングの修正"
    This reverts commit c66a758507fe0b026f43e8eee502ec5022a7a670.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   devise_for :users
   
   root to: "items#index"
 
-  resources :items, only: [:index, :create]
-
+  resources :items, only: [:index, :new, :create]
 end
