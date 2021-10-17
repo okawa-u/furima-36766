c66a758507fe0b026f43e8eee502ec5022a7a670
Okawa <pandanyanya@gmail.com>
2021-10-17 18:16:24 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :create]
+
 end
