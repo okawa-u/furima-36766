39d9a52809bcc9efe16d058f9672e930a2a7354b
Okawa <pandanyanya@gmail.com>
2021-10-17 18:19:09 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    # @item = Item.includes(:user).order(created_at: :desc)
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -15,10 +15,6 @@ class ItemsController < ApplicationController
     else
       render :new
     end
-  end
-
-  def index
-    @item = Item.includes(:user).order(created_at: :desc)
   end
 
   private
