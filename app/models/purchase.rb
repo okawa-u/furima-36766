46630fc8343c97bf04694b911bcaf55881c60a63
Okawa <pandanyanya@gmail.com>
2021-10-21 16:03:31 +0900

    Update purchase.rb

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,7 @@
 class Purchase
 
   include ActiveModel::Model
-  attr_accessor :code, :area_id, :muni, :address, :building, :phone, :user_id, :item_id, :token
+  attr_accessor :code, :area_id, :muni, :address, :building, :phone, :user_id, :item_id
   with_options  presence: true do
   validates :code, format: { with:/\A\d{3}[-]\d{4}\z/ }
   validates :area_id, numericality: { other_than: 0 , message: "を選択してください"}
@@ -10,7 +10,6 @@ class Purchase
   validates :phone, format: { with: /\d{10,11}/}
   validates :user_id
   validates :item_id
-  validates :token
   end
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
