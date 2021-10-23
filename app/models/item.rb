b73161cf47ddb01f1e073ccf1b5c1c7cfbe130d7
Okawa <pandanyanya@gmail.com>
2021-10-23 16:55:46 +0900

    エラー文の再定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,19 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :delivery_day
   has_one    :order
+
   validates :name, presence: true
   validates :image, presence: true
   validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 1, message: 'must be other than 1' }
-  validates :state_id, numericality: { other_than: 1, message: 'must be other than 1' }
-  validates :fee_id, numericality: { other_than: 1, message: 'must be other than 1' }
-  validates :area_id, numericality: { other_than: 1, message: 'must be other than 1' }
-  validates :delivery_day_id, numericality: { other_than: 1, message: 'must be other than 1' }
+  validates :category_id, numericality: { other_than: 1, message: 'いずれか一つお選びください' }
+  validates :state_id, numericality: { other_than: 1, message: 'いずれか一つお選びください' }
+  validates :fee_id, numericality: { other_than: 1, message: 'いずれか一つお選びください' }
+  validates :area_id, numericality: { other_than: 1, message: 'いずれか一つお選びください' }
+  validates :delivery_day_id, numericality: { other_than: 1, message: 'いずれか一つお選びください' }
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' }
 end
+# numericality=数値のみ(整数のみはonly_integer:trueオプションをつける)
+# other_than＝渡した値以外の値でなければならない
+# format=withオプションで与えられた正規表現と属性の値がマッチするか
