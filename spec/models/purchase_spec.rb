258ed755aa653399cc776959e2f826cce51a2381
Okawa <pandanyanya@gmail.com>
2021-10-23 15:02:34 +0900

    Rubocopの実行

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe User, type: :model do
     before do
       @user = FactoryBot.create(:user)
       @item = FactoryBot.create(:item)
-      @purchase = FactoryBot.build(:purchase, user_id: @user.id,item_id: @item.id)
+      @purchase = FactoryBot.build(:purchase, user_id: @user.id, item_id: @item.id)
       sleep 0.1
     end
 
     context '正常系' do
       it '全ての項目が入力されていれば購入できる' do
-      expect(@purchase).to be_valid
+        expect(@purchase).to be_valid
       end
       it '建物名は空でも保存できること' do
         @purchase.building = ''
@@ -28,29 +28,29 @@ RSpec.describe User, type: :model do
       it '郵便番号が半角のハイフンを含んだ形式でないと保存できない' do
         @purchase.code = '1234567'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Code is invalid")
+        expect(@purchase.errors.full_messages).to include('Code is invalid')
       end
       it '郵便番号が８桁だと保存できない' do
         @purchase.code = '123-12345'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Code is invalid")
+        expect(@purchase.errors.full_messages).to include('Code is invalid')
       end
       it '郵便番号の文字数が足りないと保存できない' do
         @purchase.code = '123-123'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Code is invalid")
+        expect(@purchase.errors.full_messages).to include('Code is invalid')
       end
       it '都道府県、選択がない場合は登録できない' do
         @purchase.area_id = 0
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Area を選択してください")
+        expect(@purchase.errors.full_messages).to include('Area を選択してください')
       end
       it '都道府県、空の場合は登録できない' do
         @purchase.area_id = nil
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Area を選択してください")
+        expect(@purchase.errors.full_messages).to include('Area を選択してください')
       end
-        it '市町が空だと保存できない' do
+      it '市町が空だと保存できない' do
         @purchase.muni = ''
         @purchase.valid?
         expect(@purchase.errors.full_messages).to include("Muni can't be blank")
@@ -68,17 +68,17 @@ RSpec.describe User, type: :model do
       it '電話番号が12桁以上だと保存できない' do
         @purchase.phone = '123456789101112'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone is too long (maximum is 11 characters)")
+        expect(@purchase.errors.full_messages).to include('Phone is too long (maximum is 11 characters)')
       end
       it '電話番号が足りない場合だと保存できない' do
-        @purchase.phone = 12345
+        @purchase.phone = 12_345
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone is invalid")
+        expect(@purchase.errors.full_messages).to include('Phone is invalid')
       end
       it '電話番号が異常な番号の場合だと保存できない' do
-        @purchase.phone = 123-123-123
+        @purchase.phone = 123 - 123 - 123
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone is invalid")
+        expect(@purchase.errors.full_messages).to include('Phone is invalid')
       end
       it 'userが紐付いていないと保存できないこと' do
         @purchase.user_id = nil
@@ -90,7 +90,7 @@ RSpec.describe User, type: :model do
         @purchase.valid?
         expect(@purchase.errors.full_messages).to include("Item can't be blank")
       end
-      it "tokenが空では登録できないこと" do
+      it 'tokenが空では登録できないこと' do
         @purchase.token = nil
         @purchase.valid?
         expect(@purchase.errors.full_messages).to include("Token can't be blank")
